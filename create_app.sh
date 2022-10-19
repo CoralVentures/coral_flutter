df8115bd97cc48f62eacc605052da6a7971c3570
Matthew Jaoudi <gadfly361@gmail.com>
2022-10-18 23:02:21 -0700

    create app inside playground dir

--- create_app.sh
+++ create_app.sh
@@ -2,16 +2,19 @@
 
 set -e
 
+mkdir -p playground
+cd playground
+
 flutter create $1
 cd $1
 
 rm -rf linux macos test web windows lib
 rm .gitignore analysis_options.yaml pubspec.yaml README.md
-cp -R ../templates/* .
-cp -R ../.vscode .
-cp ../.gitignore .
- 
-cp -R ../examples/counter_example/lib .
+
+cp -R ../../templates/* .
+cp -R ../../.vscode .
+cp ../../.gitignore .
+cp -R ../../examples/counter_example/lib .
 
 mv test/flutter_test_config.dart_change_name test/flutter_test_config.dart
 mv analysis_options.yaml_change_name analysis_options.yaml
@@ -21,3 +24,9 @@ find . -type f -name '*.dart' -exec sed -i '' s/counter_example/$1/g {} +
 sed -i '' -e "s/CHANGE_ME/$1/g" README.md
 sed -i '' -e "s/CHANGE_ME/$1/g" pubspec.yaml
 sed -i '' -e "s/CHANGE_ME/$1/g" mkdocs.yml
+
+echo "---"
+echo "Your coral_flutter application has been made."
+echo ""
+echo "  $ cd playground/$1"
+echo ""
