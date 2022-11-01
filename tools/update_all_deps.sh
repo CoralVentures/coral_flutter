877583c5f301ca4d4121f2feedbb0f13523ec18b
Matthew Jaoudi <gadfly361@gmail.com>
2022-10-31 17:07:34 -0700

    add update deps script

---
+++ tools/update_all_deps.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e 
+
+flutter pub get packages/coral_analytics_repository
+flutter pub get packages/coral_bloc
+flutter pub get packages/coral_bootstrap
+flutter pub get packages/coral_cli
+flutter pub get packages/coral_error_monitoring_repository
+flutter pub get packages/coral_logger
+flutter pub get packages/coral_poller
+flutter pub get packages/coral_tester
+
+flutter pub get examples/bottom_nav_example
+flutter pub get examples/counter_example
+flutter pub get examples/data_layer_example
+flutter pub get examples/double_yield_example
+flutter pub get examples/routing_example
